4e28a6877089ca4e744c013765bf292f2769a476
Alex Kuiper <alex.kuiper@qsd.nl>
2022-12-12 08:28:36 +0100

    Small test fix:

--- tddasm/src/test/java/net/nightwhistler/tddasm/mos65xx/OpCodeTest.java
+++ tddasm/src/test/java/net/nightwhistler/tddasm/mos65xx/OpCodeTest.java
@@ -1,25 +1,19 @@
 package net.nightwhistler.tddasm.mos65xx;
 
 import io.vavr.Tuple2;
+import io.vavr.collection.List;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class OpCodeTest {
 
     private List<OpCode> findCodesForByte(int byteValue) {
-        return Arrays.asList(OpCode.values())
-                .stream()
-                .flatMap(oc ->
-                    oc.addressingModeMappings().map( m -> new Tuple2<>(oc, m))
-                            .toJavaStream()
-                ).filter(t -> t._2.code() == (byte) byteValue)
-                .map(Tuple2::_1)
-                .collect(Collectors.toList());
+        return List.of(OpCode.values())
+                .flatMap(oc -> oc.addressingModeMappings().map(m -> new Tuple2<>(oc, m)))
+                .filter(t -> t._2.code() == (byte) byteValue)
+                .map(Tuple2::_1);
     }
 
     /**
@@ -28,7 +22,7 @@ class OpCodeTest {
      */
     @Test
     public void testUniqueCodes() {
-        for (int i=0x00; i < 0xFF; i++) {
+        for (int i=0x00; i <= 0xFF; i++) {
             List<OpCode> codes = findCodesForByte(i);
             assertTrue(codes.size() <= 1,
                     String.format("Found %d OpCodes for byte-value %s: %s",
