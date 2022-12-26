d9ca754def0f8256d2ba15185a2f7c32bb54340e
Alex Kuiper <alex.kuiper@qsd.nl>
2022-12-26 17:28:42 +0100

    Little bit of work on parsing

--- tddasm/src/main/java/net/nightwhistler/tddasm/asmparser/AsmFileParser.java
+++ tddasm/src/main/java/net/nightwhistler/tddasm/asmparser/AsmFileParser.java
@@ -1,6 +1,9 @@
 package net.nightwhistler.tddasm.asmparser;
 
 import io.vavr.collection.List;
+import net.nightwhistler.tddasm.mos65xx.OpCode;
+import net.nightwhistler.tddasm.mos65xx.Operand;
+import net.nightwhistler.tddasm.mos65xx.OperationProvider;
 import net.nightwhistler.tddasm.mos65xx.ProgramElement;
 
 import java.util.regex.Matcher;
@@ -12,15 +15,34 @@ public class AsmFileParser {
    private static final Pattern pattern = Pattern.compile("\s*([a-zA-Z]{0,3})\s*([#0-9].*)");
 
     public List<ProgramElement> parseLine(String line) {
-//        List<String> lineElements = List.of(line.split("\s"));
         Matcher matcher = pattern.matcher(line);
         if (matcher.matches()) {
-            System.out.println(matcher.group(1));
-            System.out.println(matcher.group(2));
+            String opCodeAsString = matcher.group(1);
+            String operandString = matcher.group(2);
+
+            return List.of(
+                    new OperationProvider(
+                            OpCode.valueOf(opCodeAsString.toUpperCase()),
+                            parseOperand(operandString)
+                    )
+            );
         } else {
             throw new IllegalArgumentException("Doesn't match");
         }
+    }
 
-        return List.empty();
+    private Operand parseOperand(String value) {
+        if (value.startsWith("#")) {
+            return Operand.value(parseByte(value.substring(1)));
+        }
+        throw new UnsupportedOperationException("Not supported yet: " + value);
+    }
+
+    private int parseByte(String value) {
+        if (value.startsWith("$")) {
+            return Integer.parseInt(value.substring(1), 16);
+        } else {
+            return Integer.parseInt(value);
+        }
     }
 }
--- tddasm/src/test/java/net/nightwhistler/tddasm/asmparser/AsmFileParserTest.java
+++ tddasm/src/test/java/net/nightwhistler/tddasm/asmparser/AsmFileParserTest.java
@@ -5,17 +5,15 @@ import net.nightwhistler.tddasm.mos65xx.OpCode;
 import net.nightwhistler.tddasm.mos65xx.Operand;
 import net.nightwhistler.tddasm.mos65xx.OperationProvider;
 import net.nightwhistler.tddasm.mos65xx.ProgramElement;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AsmFileParserTest {
 
-    @Disabled
     @Test
     public void testLoadValue() {
-        List<ProgramElement> element = new AsmFileParser().parseLine("lda #3");
+        List<ProgramElement> element = new AsmFileParser().parseLine("lda #$03");
         assertEquals(1, element.size());
         assertEquals(new OperationProvider(OpCode.LDA, Operand.value(3)), element.get(0));
     }
